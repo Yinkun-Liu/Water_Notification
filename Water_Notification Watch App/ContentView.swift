46d003905954d8d6a10b587f0b8824411de04c55
Yinkun-Liu <835221468@qq.com>
2023-12-16 18:20:50 +0800

    Initial Commit

---
+++ Water_Notification Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water_Notification Watch App
+//
+//  Created by Macmini on 2023/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water_Notification Watch App/Water_NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_NotificationApp.swift
+//  Water_Notification Watch App
+//
+//  Created by Macmini on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct Water_Notification_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
